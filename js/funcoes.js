240e87044f070d67c18a6213febfb37c5066de2e
WesleyVPaiva <73004684+WesleyVPaiva@users.noreply.github.com>
2020-10-16 22:59:54 -0300

    meu primeiro commit

---
+++ js/funcoes.js
@@ -0,0 +1,50 @@
+function exibir_categoria(categoria) 
+{
+
+
+  let elementos = document.getElementsByClassName('box_produtos');
+  console.log(elementos);
+  for(let i=0; i<elementos.length; i++)
+  {
+      console.log(elementos[i].id);
+      if (categoria == elementos[i].id)
+      {
+        elementos[i].style = "display:block";
+      }
+      else
+      {
+        elementos[i].style = "display:none";
+      }
+  }
+}
+
+let exibir_todos = () => {
+
+    let elementos = document.getElementsByClassName('box_produtos');
+    
+    for(let i=0; i<elementos.length; i++)
+    {
+        elementos[i].style = "display:inline-block";
+    }
+}
+
+let destaque = (imagem) => {
+    console.log(imagem);
+
+    if(imagem.width == 240)
+    {
+        imagem.width = 120;
+    }
+    else
+    {
+        imagem.width = 240;
+    }
+
+}
+
+let mouseMove = (imagem) =>
+{
+    console.log(imagem);   
+    imagem.style = "background:yellow";
+    
+}
